7f97bda4bc5ef110ea1251bb8898c4772a6fe6bc
vugutsa <vugutsamercy84@gmail.com>
2020-12-06 23:49:55 +0300

    add the class role to the models.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,36 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+
 bootstrap = Bootstrap()
+
 db = SQLAlchemy()
 
+def create_app(config_name):
+    app = Flask(__name__)
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    #from .auth import auth as auth_blueprint
+    #app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    
+    #mail.init_app(app)
+    #simple.init_app(app)
+    
+    # configure UploadSet
+    #configure_uploads(app,photos)
+    
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+ 
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    return app
 
-
-from app import views
--- config.py
+++ config.py
@@ -30,8 +30,8 @@ class DevConfig(Config):
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
    
 DEBUG = True    
