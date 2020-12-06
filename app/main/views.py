b659b3898656c543e68ed3c0c5bf090677b0b46e
vugutsa <vugutsamercy84@gmail.com>
2020-12-07 01:19:52 +0300

    import the abort function that stops a request

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,23 @@
+from flask import render_template,request,redirect,url_for,abort
+from ..models import Reviews, User
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
+
+
+
+
+
+
 # @main.route('/pitches/review/new/<int:id>', methods = ['GET','POST'])
 # @login_required
 # def new_review(id):
