bf205a93563e8bcc62e904d934ee9f66e48ab31b
vugutsa <vugutsamercy84@gmail.com>
2020-12-07 00:16:54 +0300

    change password for user moringa

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1998@localhost/pitching'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitching'
 
     
     
